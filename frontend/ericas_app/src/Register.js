f7290f3f66efeb9b83b3fa6655b8b3db8fc9b68e
Erica <Erica.Stanley@live.ca>
2018-06-07 23:09:39 -0400

    more to registry form

--- frontend/ericas_app/src/Register.js
+++ frontend/ericas_app/src/Register.js
@@ -7,7 +7,7 @@ class Register extends Component {
                 Hello?!
                   <form className= 'login container'>
                   <div className="form-group">
-                        <label htmlFor="FirstName">Name</label>
+                        <label htmlFor="FirstName"> First Name</label>
                         <input type="text" className="form-control shadow-light" id="FirstName" aria-describedby="FirstName" placeholder="First Name"/>
                     </div>
                     <div className="form-group">
@@ -30,7 +30,14 @@ class Register extends Component {
                         <label htmlFor="Password">Password</label>
                         <input type="password" className="form-control shadow-light" id="Password" placeholder="Password"/>
                     </div>
-                    
+                    <div className="form-group">
+                        <label htmlFor="Address">Address</label>
+                        <input type="text" class="form-control shadow-light" id="Address" placeholder="1234 Main St"/>
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="City">City</label>
+                        <input type="text" class="form-control shadow-light" id="City"/>
+                    </div>
                     <button type="submit" className="btn btn-danger">Submit</button>
                 </form>
             </div>
@@ -39,36 +46,3 @@ class Register extends Component {
 }
 
 export default Register;
-
-{/* <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div> */}
